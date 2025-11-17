18b609e724fb05cfc915102a1729e4accc257302
ManarAjhar <manarajhar93@gmail.com>
2025-11-17 22:54:37 +0400

    Pylint (Part 1)
    I added comments and modified the server script to modify the pylint ranking. As of this upload, it is around 8/10.

--- server.py
+++ server.py
@@ -1,66 +1,60 @@
+# Import 'Flask', 'render_template', and 'request' from 'flask'
 from flask import Flask, render_template, request
+# Import 'emotion_detector' function from 'emotion_detection'
 from EmotionDetection.emotion_detection import emotion_detector
 
+# Name the application
 app = Flask("Emotion Detector")
 
+# Set the route for the javascript page
 @app.route("/emotionDetector")
+# Define the function
 def emotion_function():
 
-    text_to_analyze = request.args.get("textToAnalyze") # Analyzing the Text
+    # Analyzing the Text
+    text_to_analyze = request.args.get("textToAnalyze")
 
-    response = emotion_detector(text_to_analyze) # Storing the Analyzed Result
+    # Storing the Analyzed Result
+    response = emotion_detector(text_to_analyze)
 
-    if response['dominant_emotion'] == None: # If the value of the key 'dominant_emotion' in 'response' is equal to 'None', then...
-        return "Invalid text! Please try again!" # Terminamte the function by returning a default string
+    # If the value of the key 'dominant_emotion' is equal to 'None', then...
+    if response['dominant_emotion'] is None:
+        # Terminamte the function by returning a default string
+        return "Invalid text! Please try again!"
 
     # 1. Storing Anger's Score
-
     anger_score = response['anger']
-
     highest_score = anger_score
-
     dominant_emotion = 'anger'
-
     # 2. Storing and Evaluating Disgust's Score
-
     disgust_score = response['disgust']
-
     if disgust_score > highest_score:
         highest_score = disgust_score
         dominant_emotion = 'disgust'
-    
     # 3. Storing and Evaluating Fear's Score
-    
     fear_score = response['fear']
-
     if fear_score > highest_score:
         highest_score = fear_score
         dominant_emotion = 'fear'
-    
     # 4. Storing and Evaluating Joy's Score
-
     joy_score = response['joy']
-
     if joy_score > highest_score:
         highest_score = joy_score
         dominant_emotion = 'joy'
-    
     # 5. Storing and Evaluating Sadness's Score
-
     sadness_score = response['sadness']
-
     if sadness_score > highest_score:
         highest_score = sadness_score
         dominant_emotion = 'sadness'
-
     # Returning the final text
-
     return "For the given statement, the system response is 'anger': {}, 'disgust': {}, 'fear': {}, 'joy': {} and 'sadness': {}. The dominant emotion is {}.".format(anger_score, disgust_score, fear_score, joy_score, sadness_score, dominant_emotion)
 
-
+#Set the route of the app
 @app.route("/")
-def render_index_page():
+def render_index_page(): # Define the rendering
+    # Render the interface on the 'index.html' page
     return render_template("index.html")
 
-if __name__ == "__main__":
+if __name__ == "__main__": # If this script is the main file, then...
+    # Run the app
     app.run(host="0.0.0.0", port=5000)
